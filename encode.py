d1db87c677f8b0871ccb8072e06f9d12fe15338b
lukeskywakeri <carlosbenavides628@gmail.com>
2023-10-25 16:43:53 -0400

    It is now updated

--- encode.py
+++ encode.py
@@ -8,6 +8,18 @@ def password_encoder(password):
     return encoded_password
 
 def main():
-    input_password = input("What's the password you want to encode?: ")
-    encoded_password = password_encoder(input_password)
+    print('Menu')
+    print("-------------")
+    print("1. Encode\n2. Decode\n3. Quit\n")
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        input_password = input("What's the password you want to encode?: ")
+        encoded_password = password_encoder(input_password)
+        print("Your password has been encoded and stored!")
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+
+
 main()
