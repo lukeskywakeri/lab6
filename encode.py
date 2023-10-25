3acc750a214c7d7a547c70d89d69fb419ff60279
AutumnNix <nixa@ufl.edu>
2023-10-25 17:25:10 -0400

    Added decoding function

--- encode.py
+++ encode.py
@@ -7,6 +7,26 @@ def password_encoder(password):
         encoded_password += str(encoded_digit)
     return encoded_password
 
+# Autumn Nix's decoding
+def decode(encoded_password):
+    split_password = []
+    split_password.extend(encoded_password)
+    decoded_password = ''
+    for element in split_password:
+        element = int(element)
+        if element <= 2:
+            if element == 2:
+                element = 9
+            elif element == 1:
+                element = 8
+            elif element == 0:
+                element = 7
+        else:
+            element -= 3
+        element = str(element)
+        decoded_password += element
+    return decoded_password
+
 while True:
     print('Menu')
     print("-------------")
@@ -17,8 +37,7 @@ while True:
         encoded_password = password_encoder(input_password)
         print("Your password has been encoded and stored!")
     elif option == 2:
-        pass
+        decoded_password = decode(encoded_password)
+        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
     elif option == 3:
         pass
-
-
